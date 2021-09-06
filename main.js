3e99b0b621b35ae7dbeb54f1162a6b957dba7178
tvisharoymenon <73825415+tvisharoymenon@users.noreply.github.com>
2021-09-06 14:52:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ function setup(){
 }
 function draw(){
     image( video,0,0,480,380);
+    if (status != ""){
+        objectDetector.detect(video,gotResult)
+    
+    for (i = 0; i<objects.length; i++){
+        doctument.getElementById("status").innerHTML = "Status : Objects Detected";
+        doctument.getElementById("numberofobjects").innerHTML = "number of objects detected are:"+ objects.lenght;
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label+" "+percent+"%",objects[i].x + 15, objects[i].y+ 15);
+    noFill();
+stroke("#FF0000");
+rect(objects[i].x,objects[i].y,objects[i].width, objects[i].height)   }
+}
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd',modelloaded);
